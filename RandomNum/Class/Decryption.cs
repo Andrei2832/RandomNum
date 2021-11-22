94e68b92765dcff9f4fbf3dcf812737eb593f69d
Andrei <Andrei@DESKTOP-MUMVP0Q>
2021-11-22 15:33:31 +0300

    Добавьте файлы проекта.

---
+++ RandomNum/Class/Decryption.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNum.Class
+{
+    class Decryption
+    {
+        static public string[] Dec()
+        {
+            string path = @"C:data.txt";
+            string[] falseret = new string[1];
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string date = sr.ReadToEnd();
+                    string[] dateArray = date.Split('呈');
+                    string[] result = new string[dateArray.Length];
+
+                    ushort Key = 0x5463;
+                    for (int i = 0; i < dateArray.Length; i++)
+                    {
+                        result[i] = EncodeDecrypt(dateArray[i], Key);
+                    }
+                    result = result.Where(x => x != null).ToArray();
+                    return result;
+                }
+            }
+            catch (Exception )
+            {
+                return falseret;
+            }
+
+            
+
+
+
+        }
+        public static string EncodeDecrypt(string str, ushort secretKey)
+        {
+            var ch = str.ToArray();
+            string newStr = "";
+            foreach (var c in ch)
+                newStr += TopSecret(c, secretKey);
+            return newStr;
+        }
+
+        public static char TopSecret(char character, ushort secretKey)
+        {
+            character = (char)(character ^ secretKey);
+            return character;
+        }
+    }
+}
---
+++ RandomNum/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using RandomNum.Class;
+
+namespace RandomNum
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        public string[] date;
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            date = Decryption.Dec();
+            if (date.Length == 1)
+            {
+                MessageBox.Show("Ошибка");
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void BGeneration_Click(object sender, RoutedEventArgs e)
+        {
+            if (i == 9)
+            {
+                MessageBox.Show("Уже было выведено 10 знач");
+                textGen.Text = "";
+                return;
+            }
+            anim();
+            i++;
+
+        }
+
+        async void anim()
+        {
+            BGeneration.IsEnabled = false;
+            int[] value = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                value[i] = rnd.Next(1,11);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                textGen.Text = value[i].ToString();
+                await Task.Delay(125);
+            }
+            textGen.Text = date[i];
+            BGeneration.IsEnabled = true;
+
+        }
+
+    }
+}
